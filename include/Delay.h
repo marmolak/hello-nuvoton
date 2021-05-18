c3ab98e5cad9c99ace0790cadac7638c3442f23a
Robin Hack <hack.robin@gmail.com>
2021-05-18 15:11:21 +0200

    Initial release.

---
+++ include/Delay.h
@@ -0,0 +1,13 @@
+#ifndef DELAY_H
+#define DELAY_H
+
+void Timer0_Delay100us(UINT32 u32CNT);
+void Timer0_Delay1ms(UINT32 u32CNT);
+void Timer1_Delay10ms(UINT32 u32CNT);
+void Timer2_Delay500us(UINT32 u32CNT);
+void Timer3_Delay100ms(UINT32 u32CNT);
+
+void Timer0_Delay40ms(UINT32 u32CNT);
+void Timer3_Delay10us(UINT32 u32CNT);
+
+#endif /* DELAY_H */
---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include "N76E003.h"
+
+/*
+    Basic blink program which can be used as a hello world for nuvoton N76E003 dev board.
+    Some header files has been ported to sdcc from KEIL or similar strange C compiler.
+    Die KEIL! Die!
+*/
+
+#define DELAY_LED_MS 2000
+
+void setup()
+{
+    P15_PushPull_Mode;
+}
+
+void main(void)
+{
+    setup();
+
+    for (;;)
+    {
+        set_P15;
+        Timer0_Delay1ms(DELAY_LED_MS);
+        clr_P15;
+        Timer0_Delay1ms(DELAY_LED_MS);   
+    }
+}
